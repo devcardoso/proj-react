0e25ad55d4aa7dcffebbc21e4beb5789fd8dfd68
Henrique Santana <play.henriquesantana@gmail.com>
2019-12-10 15:27:07 -0300

    Primeiro commit

---
+++ src/components/Form.js
@@ -0,0 +1,55 @@
+
+import React, { useState } from 'react'
+
+export default function Form() {
+
+    const [firstName, setFirstName] = useState(null);
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(firstName)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+                placeholder="First name"
+                type="text"
+                name="firstName"
+                required />
+
+            <input
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+                placeholder="Last name"
+                type="text"
+                name="lastName"
+                required />
+
+            <input
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Email address"
+                type="email"
+                name="email"
+                required />
+
+            <input
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                placeholder="Password"
+                type="password"
+                name="password"
+                required />
+
+            <button type="submit">Submit</button>
+            <br />
+            <span>{firstName}</span>
+        </form>
+    )
+}
