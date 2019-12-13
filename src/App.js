792af56b79b7fd0c404bde9258c844e23aa6fbd1
Henrique Santana <play.henriquesantana@gmail.com>
2019-12-13 11:51:09 -0300

    Adicionado novos componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form'
+import TLabel from './components/ui/TLabel'
 
 function App() {
   return (
-    <div className="App">
-      <Form/>
+    <div className="container-fluid">
+      <div className="form-row">
+        <div className="col-12 col-sm-3">
+            <TLabel text={'Informe o nome'}/>
+            <input type="text" className="form-control"/>
+        </div>
+      </div>
+     
     </div>
   );
 }
